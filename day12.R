30184aceff7928075012a46ef9ddd2415e54ec38
Werth <nwerth@PA.LCL>
2023-01-03 15:40:26 -0500

    Start paths from beginning and end, to meet in the middle

--- day12.R
+++ day12.R
@@ -8,7 +8,8 @@ parse_map <- function(text_lines) {
 }
 
 
-connected_squares <- function(map) {
+connected_squares <- function(map, direction = c("forward", "backward")) {
+  direction <- match.arg(direction)
   map_int <- matrix(match(map, letters), nrow = nrow(map), ncol = ncol(map))
   map_int[map == "S"] <- 1
   map_int[map == "E"] <- 26
@@ -34,79 +35,128 @@ connected_squares <- function(map) {
     x > 0 & x <= ncol(map_int) & y > 0 & y <= nrow(map_int)
   ]
   setnames(adjacencies, old = "ii", new = "ii2")
-  cons <- map_long[
-    adjacencies, on = c("x", "y")
-  ][
-    abs(value - value2) <= 1,
-    list(ii, ii2)
-  ]
+  cons <- map_long[adjacencies, on = c("x", "y")]
+  cons <- switch(direction,
+    "forward" = cons[value + 1 >= value2, list(ii, ii2)],
+    "backward" = cons[value2 + 1 >= value, list(ii, ii2)]
+  )
   setkeyv(cons, c("ii", "ii2"))
   cons
 }
 
 
-shortest_path <- function(map) {
-  connections <- connected_squares(map)
-  start_ii <- which(map == "S")
-  end_ii <- which(map == "E")
+begin_paths <- function(map, origin = c("S", "E")) {
+  origin <- match.arg(origin)
+  direction <- switch(origin, "S" = "forward", "E" = "backward")
+  origin_index <- which(map == origin)
   paths <- data.table(
-    history_str = as.character(start_ii),
-    current = start_ii,
-    previous = start_ii,
-    n = 1L,
+    history_str = as.character(origin_index),
+    current = origin_index,
+    previous = origin_index,
+    n = 0L,
     path_id = "1"
   )
-  history <- paths[, list(path_id, visited = current)]
-  while (TRUE) {
-    paths <- paths[
-      connections,
-      on = c(current = "ii"),
-      allow.cartesian = TRUE,
-      nomatch = 0
-    ][
-      , parent_path := path_id
-    ][
-      duplicated(path_id),
-      path_id := NA
-    ]
-    na_pid <- which(is.na(paths[["path_id"]]))
-    new_pids <- seq(
-      max(as.integer(paths[["path_id"]]), na.rm = TRUE) + 1,
-      length.out = length(na_pid)
+  setattr(paths, "history", paths[["current"]])
+  setattr(paths, "connections", connected_squares(map, direction))
+  setattr(paths, "pruned", paths[0])
+  paths
+}
+
+
+extend_paths <- function(paths) {
+  history <- attr(paths, "history")
+  connections <- attr(paths, "connections")
+  pruned <- attr(paths, "pruned")
+  paths <- connections[
+    paths,
+    on = c(ii = "current"),
+    allow.cartesian = TRUE,
+  ][
+    duplicated(path_id),
+    path_id := NA
+  ]
+  setnames(paths, "ii", "current")
+  na_pid <- which(is.na(paths[["path_id"]]))
+  new_pids <- seq(
+    max(as.integer(paths[["path_id"]]), na.rm = TRUE) + 1,
+    length.out = length(na_pid)
+  )
+  new_pids <- formatC(new_pids, format = "d")
+  set(paths, i = na_pid, j = "path_id", value = new_pids)
+  paths[
+    ,
+    ":="(
+      history_str = paste(history_str, ii2, sep = ","),
+      previous = current,
+      current = ii2,
+      n = n + 1L,
+      ii2 = NULL
     )
-    new_pids <- formatC(new_pids, format = "d")
-    set(paths, i = na_pid, j = "path_id", value = new_pids)
-    paths[
-      history,
-      on = c(parent_path = "path_id", ii2 = "visited"),
-      been_here := TRUE
-    ]
-    paths <- paths[is.na(been_here)]
-    paths[
-      ,
-      ":="(
-        history_str = paste(history_str, ii2, sep = ","),
-        previous = current,
-        current = ii2,
-        n = n + 1L
-      )
-    ]
-    if (!anyNA(paths[["been_here"]])) {
-      stop("All paths will double back?!")
+  ]
+  # Remove paths that double back or rejoin a shorter path
+  been_here <- paths[["current"]] %in% history
+  # Keep only one path for each current location
+  redundant <- duplicated(paths[["current"]])
+  needs_pruned <- been_here | redundant
+  new_pruned <- paths[needs_pruned, names(pruned), with = FALSE]
+  pruned <- funion(pruned, new_pruned)
+  paths <- paths[!needs_pruned]
+  history <- union(history, paths[["current"]])
+  setattr(paths, "history", history)
+  setattr(paths, "connections", connections)
+  setattr(paths, "pruned", pruned)
+  paths
+}
+
+
+shortest_path <- function(map, max_steps = length(map)) {
+  start_paths <- begin_paths(map, "S")
+  end_paths <- begin_paths(map, "E")
+  no_path_result <- start_paths[0, list(n, path_id, history_str)]
+  while (TRUE) {
+    if (any(start_paths[["current"]] %in% end_paths[["current"]])) break
+    start_paths <- extend_paths(start_paths)
+    if (nrow(start_paths) == 0) return(no_path_result)
+    if (any(start_paths[["current"]] %in% end_paths[["current"]])) break
+    end_paths <- extend_paths(end_paths)
+    if (nrow(end_paths) == 0) return(no_path_result)
+    # Escape when reality breaks down
+    path_length <- min(start_paths[["n"]]) + min(end_paths[["n"]])
+    if (path_length > length(map)) {
+      stop(sprintf("Could not find a path shorter than traversing the whole map"))
     }
-    history <- unique(rbind(
-      history,
-      history[
-        paths, on = c(path_id = "parent_path"), allow.cartesian = TRUE
-      ][
-        ,
-        list(visited = c(visited, current[1])),
-        by = list(path_id = i.path_id)
-      ]
-    ))
-    set(paths, j = c("ii2", "parent_path", "been_here"), value = NULL)
+    if (path_length > max_steps) return(no_path_result)
   }
-  paths[current == end_ii]
+  final_paths <- start_paths[
+    end_paths,
+    on = "current",
+    nomatch = 0,
+    list(
+      n = n + i.n,
+      path_id = paste0(path_id, ".", i.path_id),
+      start_history = history_str,
+      end_history = i.history_str
+    )
+  ][
+    ,
+    end_history_rev := vapply(
+      X = strsplit(end_history, ","),
+      FUN = function(x) paste0(rev(x), collapse = ","),
+      FUN.VALUE = character(1)
+    )
+  ][
+    ,
+    ":="(
+      history_str = paste0(start_history, ",", end_history_rev),
+      start_history = NULL,
+      end_history = NULL,
+      end_history_rev = NULL
+    )
+  ]
+  final_paths
+}
+    }
+  }
 }
 
 
@@ -120,6 +170,7 @@ example_inlines <- strsplit(example_input, "\n")[[1]]
 example_map <- parse_map(example_inlines)
 example_path <- shortest_path(example_map)
 print(example_path[["n"]][1])
+stopifnot(example_path[["n"]][1] == 31)
 
 # Part 1
 letter_map <- parse_map(readLines("day12.txt"))
